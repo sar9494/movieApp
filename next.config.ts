64c4234a65d1e0e51f3ce6b5707ddfaf5b296f48
sar9484 <batbayarsaruul0@gmail.com>
2025-02-01 01:18:04 +0800

    fkadjg

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     BASE_URL: process.env.BASE_URL || "",
+    API_KEY: process.env.API_KEY || "",
+    IMAGE_BASE_URL: process.env.IMAGE_BASE_URL || "",
   },
 };
 
--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,2 +1,9 @@
 import axios from "axios"
 const BASE_URL = process.env.BASE_URL;
+const IMAGE_BASE_URL = process.env.IMAGE_BASE_URL;
+const API_KEY = process.env.API_KEY;
+
+const getMoviesInfo = (page:number,name:string) =>{
+    return axios.get(`${BASE_URL}/movie/${name}?language=en-US&page=${page}&api_key=${API_KEY}`)
+}
+
